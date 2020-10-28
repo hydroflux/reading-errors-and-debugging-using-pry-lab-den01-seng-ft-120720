a27fdc5dbcbca68ad42d628570ebf5cfa47d1b36
Jack Hubert <jphubert93@gmail.com>
2020-10-28 18:01:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,14 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    binding.pry
+    10.times do
+      string += 's'
+    end
+    # 10 * "s" = string
+    binding.pry
   else
     string
   end
